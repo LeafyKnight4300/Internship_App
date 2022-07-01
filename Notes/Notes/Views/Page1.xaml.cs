2fef95b82f18c9277eeca922e63f845fafead1c6
Sern_Intern <Sern_Intern@SERN-INTERN43.SERN.MIL>
2022-07-01 16:21:46 -0400

    the other files

---
+++ Notes/Notes/Views/Page1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace Notes.Views
+{
+    public partial class Page1 : ContentPage
+    {
+        
+       
+
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        async void OnDisplayAlertButtonClicked(object sender, EventArgs e)
+        {
+            bool start = await DisplayAlert("hello random person", "My name is William, and odds are im taller than you", "Gotcha", "Doubt it");
+            if (start == false)
+            {
+              await DisplayAlert("Bruh","", null, "¯|_ (ツ)_|¯");     
+            }
+
+        }
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            // Launch the specified URL in the system browser.
+            await Launcher.OpenAsync("https://m.twitch.tv/videos/1519067637");
+        }
+
+
+    }
+
+
+      
+        
+
+}
---
+++ Notes/Notes/Views/Page2.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+    }
+}
