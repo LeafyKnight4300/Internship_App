3a541605e8c861ee9313382c59208e41bc411252
LeafyKnight4300 <74797797+LeafyKnight4300@users.noreply.github.com>
2022-07-07 08:47:31 -0400

    touched up a lot of the files (#10)
    Co-authored-by: Sern_Intern <Sern_Intern@SERN-INTERN43.SERN.MIL>

--- Notes/Notes/Views/NotesPage.xaml.cs
+++ Notes/Notes/Views/NotesPage.xaml.cs
@@ -13,26 +13,18 @@ namespace Notes.Views
             InitializeComponent();
 
             // Read the file.
-            if (File.Exists(_fileName))
-            {
-                editor.Text = File.ReadAllText(_fileName);
-            }
+            
         }
 
         void OnSaveButtonClicked(object sender, EventArgs e)
         {
             // Save the file.
-            File.WriteAllText(_fileName, editor.Text);
+            
         }
 
         void OnDeleteButtonClicked(object sender, EventArgs e)
         {
-            // Delete the file.
-            if (File.Exists(_fileName))
-            {
-                File.Delete(_fileName);
-            }
-            editor.Text = string.Empty;
+           
         }
     }
 }
--- Notes/Notes/obj/Debug/netstandard2.0/Views/NotesPage.xaml.g.cs
+++ Notes/Notes/obj/Debug/netstandard2.0/Views/NotesPage.xaml.g.cs
@@ -17,12 +17,8 @@ namespace Notes.Views {
     public partial class NotesPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.Editor editor;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(NotesPage));
-            editor = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Editor>(this, "editor");
         }
     }
 }
